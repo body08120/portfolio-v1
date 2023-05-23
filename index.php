3a0f83b476642ce781ff0e99732b2e1c7ba85358
body08120 <nataelrenollet08120@gmail.com>
2023-05-23 22:07:10 +0200

    ajout des sections: header, projects, vitrines, compétences, et footer /// manque footer

---
+++ index.php
@@ -0,0 +1,189 @@
+<!DOCTYPE html>
+<html lang="fr">
+
+<head>
+  <meta charset="UTF-8" />
+  <meta http-equiv="X-UA-Compatible" content="IE=edge" />
+  <meta name="viewport" content="width=device-width, initial-scale=1.0" />
+  <title>Renollet Nataël - Développeur web</title>
+
+  <link rel="stylesheet" href="assets/css/vitrine.css">
+</head>
+
+<body>
+  <!--HEADER SECTION-->
+  <header>
+    <div>
+      <div>
+        <h1>Nataël RENOLLET</h1>
+        <p>
+          Je suis passionné par internet et le web, je suis actuellement
+          apprenti développeur web et web mobile à Simplon au pôle formation
+          UIMM de Charleville-Mézières. J'aime apprendre en continu et suis
+          toujours à la recherche de nouvelles compétences à acquérir pour
+          améliorer mes capacités en programmation. Mon objectif est d'obtenir
+          mon diplôme et d'évoluer dans la même structure en tant que
+          "Concepteur Développeur d'application".
+        </p>
+        <a href="#contact">Contactez-moi</a>
+      </div>
+      <div>
+        <img src="assets/img/profil.png" alt="Photo de profil" />
+      </div>
+    </div>
+    <?php include('includes/socialicons.php'); ?>
+  </header>
+  <!--END HEADER SECTION-->
+
+  <!--PROJECTS BLOC-->
+  <section>
+    <h2>Je créer avec passion !</h2>
+    <p>
+      La passion est l'essence même de ma pratique en tant que développeur.
+      Chaque projet est une occasion de me surpasser, d'apprendre de nouvelles
+      compétences et de relever de nouveaux défis. Ma passion pour la création
+      ne se limite pas seulement au développement, mais se reflète également
+      dans mon approche globale de la vie professionnelle !
+    </p>
+    <a href="#project">Découvrez ce que j'ai fais</a>
+  </section>
+  <!--END PROJECTS BLOC-->
+
+  <!--VITRINE SECTION-->
+  <section>
+    <div class="box-container">
+
+      <div class="box-item">
+        <div class="flip-box">
+          <div class="flip-box-front text-center"
+            style="background-image: url('https://s25.postimg.cc/frbd9towf/cta-2.png');">
+            <div class="inner color-white">
+              <h3 class="flip-box-header">Custom Domains</h3>
+              <p>A short sentence describing this callout is.</p>
+              <img src="https://s25.postimg.cc/65hsttv9b/cta-arrow.png" alt="" class="flip-box-img">
+            </div>
+          </div>
+          <div class="flip-box-back text-center"
+            style="background-image: url('https://s25.postimg.cc/frbd9towf/cta-2.png');">
+            <div class="inner color-white">
+              <h3 class="flip-box-header">Custom Domains</h3>
+              <p>A short sentence describing this callout is.</p>
+              <button class="flip-box-button">Learn More</button>
+            </div>
+          </div>
+        </div>
+      </div>
+
+      <div class="box-item">
+        <div class="flip-box">
+          <div class="flip-box-front text-center"
+            style="background-image: url('https://s25.postimg.cc/hj4c4qnov/cta-3.png');">
+            <div class="inner color-white">
+              <h3 class="flip-box-header">Never Sleeps</h3>
+              <p>A short sentence describing this callout is.</p>
+              <img src="https://s25.postimg.cc/65hsttv9b/cta-arrow.png" alt="" class="flip-box-img">
+            </div>
+          </div>
+          <div class="flip-box-back text-center"
+            style="background-image: url('https://s25.postimg.cc/hj4c4qnov/cta-3.png');">
+            <div class="inner color-white">
+              <h3 class="flip-box-header">Never Sleeps</h3>
+              <p>A short sentence describing this callout is.</p>
+              <button class="flip-box-button">Learn More</button>
+            </div>
+          </div>
+        </div>
+      </div>
+
+      <div class="box-item">
+        <div class="flip-box">
+          <div class="flip-box-front text-center filter-"
+            style="background-image: url('https://s25.postimg.cc/l2q9ujy4f/cta-4.png');">
+            <div class="inner color-white">
+              <h3 class="flip-box-header">Dedicated</h3>
+              <p>A short sentence describing this callout is.</p>
+              <img src="https://s25.postimg.cc/65hsttv9b/cta-arrow.png" alt="" class="flip-box-img">
+            </div>
+          </div>
+          <div class="flip-box-back text-center"
+            style="background-image: url('https://s25.postimg.cc/l2q9ujy4f/cta-4.png');">
+            <div class="inner color-white">
+              <h3 class="flip-box-header">Dedicated</h3>
+              <p>A short sentence describing this callout is.</p>
+              <button class="flip-box-button">Learn More</button>
+            </div>
+          </div>
+        </div>
+      </div>
+
+    </div>
+  </section>
+  <!--END VITRINE SECTION-->
+
+  <!--COMPETENCES SECTION-->
+  <section>
+    <a href="assets/img/cv.pdf" target="blank">
+      <img src="assets/img/cv.png" alt="Visualisation de mon CV" />
+    </a>
+
+    <h2>Mes compétences</h2>
+    <ul>
+
+      <li>
+        <h3>Perfectionniste</h3>
+        <p>J'ai le sens du détail et j'aime que les choses soit au rendu final exactement comme elles étaient prévues à
+          la base.</p>
+      </li>
+
+      <li>
+        <h3>Perfectionniste</h3>
+        <p>J'ai le sens du détail et j'aime que les choses soit au rendu final exactement comme elles étaient prévues à
+          la base.</p>
+      </li>
+
+      <li>
+        <h3>Perfectionniste</h3>
+        <p>J'ai le sens du détail et j'aime que les choses soit au rendu final exactement comme elles étaient prévues à
+          la base.</p>
+      </li>
+
+      <li>
+        <h3>Perfectionniste</h3>
+        <p>J'ai le sens du détail et j'aime que les choses soit au rendu final exactement comme elles étaient prévues à
+          la base.</p>
+      </li>
+
+      <li>
+        <h3>Perfectionniste</h3>
+        <p>J'ai le sens du détail et j'aime que les choses soit au rendu final exactement comme elles étaient prévues à
+          la base.</p>
+      </li>
+
+      <li>
+        <h3>Perfectionniste</h3>
+        <p>J'ai le sens du détail et j'aime que les choses soit au rendu final exactement comme elles étaient prévues à
+          la base.</p>
+      </li>
+
+    </ul>
+  </section>
+  <!--END --COMPETENCES SECTION-->
+
+  <!--CONTACT SECTION-->
+  <section></section>
+  <!--END CONTACT SECTION-->
+
+  <!--FOOTER SECTION-->
+  <!-- <footer>
+    <span>Portfolio 2023</span>
+    <br />
+    <span>© 2023 - RENOLLET Nataël</span> 
+    <br />
+    <span>Mentions Légales</span> -->
+
+  <?php // include('includes/socialicons.php'); ?>
+  <!-- </footer>s -->
+  <!--END FOOTER SECTION-->
+</body>
+
+</html>
