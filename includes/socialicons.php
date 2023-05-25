fa6bf1eb83e9835fc20d47c19dd34b1d270f439d
body08120 <nataelrenollet08120@gmail.com>
2023-05-25 22:35:08 +0200

    background ok, header quasiment ok, revoir lineheight et responsive :)

--- includes/socialicons.php
+++ includes/socialicons.php
@@ -1,13 +1,20 @@
-<div>
+<div class="mx-auto py-10 flex justify-evenly 
+            xl:w-3/6">
   <a href="https://www.linkedin.com/">
-    <img src="assets/img/icons/linkedin.png" alt="Logo Linkedin" />
+    <img src="assets/img/icons/linkedin.png" alt="Logo Linkedin"
+      class="h-10 
+            xl:h-16 xl:hover:border-2 xl:hover:border-primary xl:hover:rounded-full xl:hover:h-16" />
   </a>
 
   <a href="https://www.instagram.com/">
-    <img src="assets/img/icons/instagram.png" alt="Logo Instagram" />
+    <img src="assets/img/icons/instagram.png" alt="Logo Instagram"
+      class="h-10 
+            xl:h-16 xl:hover:border-2 xl:hover:border-primary xl:hover:rounded-full xl:hover:h-16" />
   </a>
 
   <a href="https://www.facebook.com/">
-    <img src="assets/img/icons/facebook.png" alt="Logo Facebook" />
+    <img src="assets/img/icons/facebook.png" alt="Logo Facebook"
+      class="h-10 
+            xl:h-16 xl:hover:border-2 xl:hover:border-primary xl:hover:rounded-full xl:hover:h-16" />
   </a>
 </div>
--- index.php
+++ index.php
@@ -6,194 +6,216 @@
   <meta http-equiv="X-UA-Compatible" content="IE=edge" />
   <meta name="viewport" content="width=device-width, initial-scale=1.0" />
   <title>Renollet Nataël - Développeur web</title>
+  <!--description-->
 
-  <link rel="stylesheet" href="assets/css/vitrine.css">
+  <!-- Tailwind CSS -->
+  <script src="https://cdn.tailwindcss.com"></script>
+  <script>
+    tailwind.config = {
+      theme: {
+        extend: {
+          colors: {
+            "primary": "#b8a28e",
+            "secondary": "#ffffff",
+            "default": "#36422E"
+          }
+        }
+      }
+    }
+  </script>
+  <style type="text/tailwindcss">
+    @layer utilities {
+      .bg-header {
+        background: linear-gradient(rgba(1, 1, 1, 0.1), rgba(1, 1, 1, 0.1)), url('assets/img/bg/background-header.png');
+        background-size: cover;
+        background-repeat: no-repeat;
+        background-position: center;
+        background-attachment: fixed;
+        /* -webkit-background-size: cover;
+        -moz-background-size: cover;
+        -o-background-size: cover; */
+      }
+        .bg-page {
+        background: linear-gradient(rgba(1, 1, 1, 0.1), rgba(1, 1, 1, 0.1)), url('assets/img/bg/background-page.png'); 
+        background-size: cover;
+        background-repeat: no-repeat;
+        background-position: coutain;
+        background-attachment: fixed;
+        /* filter: brightness(50%) */
+        /* -webkit-background-size: cover;
+        -moz-background-size: cover;
+        -o-background-size: cover; */
+      }
+      .bg-end {
+        background: linear-gradient(rgba(1, 1, 1, 0.1), rgba(1, 1, 1, 0.1)), url('assets/img/bg/background-end.png'); 
+        background-size: cover;
+        background-repeat: no-repeat;
+        background-position: coutain;
+        background-attachment: fixed;
+        /* filter: brightness(50%) */
+        /* -webkit-background-size: cover;
+        -moz-background-size: cover;
+        -o-background-size: cover; */
+      }
+
+      .h1-style{
+        color: #76614F;
+        font-size: 28px;
+        letter-spacing: 0.2em;
+        font-weight: 300;
+      }
+    }
+  </style>
+
 </head>
 
 <body>
-  <!--HEADER SECTION-->
-  <header>
-    <div>
-      <div>
-        <h1>Nataël RENOLLET</h1>
-        <p>
-          Je suis passionné par internet et le web, je suis actuellement
-          apprenti développeur web et web mobile à Simplon au pôle formation
-          UIMM de Charleville-Mézières. J'aime apprendre en continu et suis
-          toujours à la recherche de nouvelles compétences à acquérir pour
-          améliorer mes capacités en programmation. Mon objectif est d'obtenir
-          mon diplôme et d'évoluer dans la même structure en tant que
-          "Concepteur Développeur d'application".
-        </p>
-        <a href="#contact">Contactez-moi</a>
-      </div>
-      <div>
-        <img src="assets/img/profil.png" alt="Photo de profil" />
-      </div>
-    </div>
-    <?php include('includes/socialicons.php'); ?>
-  </header>
-  <!--END HEADER SECTION-->
+  <main class="text-primary">
+    <!--HEADER SECTION-->
+    <header class="bg-header px-10 pt-32 
+                   sm:pt-52 
+                   xl:pt-64">
 
-  <!--PROJECTS BLOC-->
-  <section>
-    <h2>Je créer avec passion !</h2>
-    <p>
-      La passion est l'essence même de ma pratique en tant que développeur.
-      Chaque projet est une occasion de me surpasser, d'apprendre de nouvelles
-      compétences et de relever de nouveaux défis. Ma passion pour la création
-      ne se limite pas seulement au développement, mais se reflète également
-      dans mon approche globale de la vie professionnelle !
-    </p>
-    <a href="#project">Découvrez ce que j'ai fais</a>
-  </section>
-  <!--END PROJECTS BLOC-->
+      <div class="flex 
+                  max-sm:flex-col-reverse
+                  lg:justify-around">
 
-  <!--VITRINE SECTION-->
-  <section>
-    <div class="box-container">
+        <div class="max-sm:text-center 
+                    md:text-justify md:w-2/3
+                    lg:w-3/5
+                    xl:w-2/5">
 
-      <div class="box-item">
-        <div class="flip-box">
-          <div class="flip-box-front text-center"
-            style="background-image: url('https://s25.postimg.cc/frbd9towf/cta-2.png');">
-            <div class="inner color-white">
-              <h3 class="flip-box-header">Custom Domains</h3>
-              <p>A short sentence describing this callout is.</p>
-              <img src="https://s25.postimg.cc/65hsttv9b/cta-arrow.png" alt="" class="flip-box-img">
-            </div>
-          </div>
-          <div class="flip-box-back text-center"
-            style="background-image: url('https://s25.postimg.cc/frbd9towf/cta-2.png');">
-            <div class="inner color-white">
-              <h3 class="flip-box-header">Custom Domains</h3>
-              <p>A short sentence describing this callout is.</p>
-              <button class="flip-box-button">Learn More</button>
-            </div>
-          </div>
+          <h1 class="h1-style">Nataël RENOLLET</h1>
+          <p>
+            Je suis passionné par internet et le web, je suis actuellement
+            apprenti développeur web et web mobile à Simplon au pôle formation
+            UIMM de Charleville-Mézières. J'aime apprendre en continu et suis
+            toujours à la recherche de nouvelles compétences à acquérir pour
+            améliorer mes capacités en programmation. Mon objectif est d'obtenir
+            mon diplôme et d'évoluer dans la même structure en tant que
+            "Concepteur Développeur d'application".
+          </p>
+          <br>
+          <a href="#contact" class="bg-default p-2 rounded-full uppercase tracking-wider hover:font-semibold">Contactez-moi</a>
+        </div>
+        <div>
+          <img src="assets/img/profil.png" alt="Photo de profil" class="max-h-64 
+                                                                        md:max-h-72
+                                                                        xl:max-h-96" />
         </div>
       </div>
+      <?php include('includes/socialicons.php'); ?>
+    </header>
+    <!--END HEADER SECTION-->
 
-      <div class="box-item">
-        <div class="flip-box">
-          <div class="flip-box-front text-center"
-            style="background-image: url('https://s25.postimg.cc/hj4c4qnov/cta-3.png');">
-            <div class="inner color-white">
-              <h3 class="flip-box-header">Never Sleeps</h3>
-              <p>A short sentence describing this callout is.</p>
-              <img src="https://s25.postimg.cc/65hsttv9b/cta-arrow.png" alt="" class="flip-box-img">
-            </div>
-          </div>
-          <div class="flip-box-back text-center"
-            style="background-image: url('https://s25.postimg.cc/hj4c4qnov/cta-3.png');">
-            <div class="inner color-white">
-              <h3 class="flip-box-header">Never Sleeps</h3>
-              <p>A short sentence describing this callout is.</p>
-              <button class="flip-box-button">Learn More</button>
-            </div>
-          </div>
-        </div>
-      </div>
+    <!--PROJECTS BLOC-->
+    <section class="bg-page px-10">
+      <h2>Je créer avec passion !</h2>
+      <p>
+        La passion est l'essence même de ma pratique en tant que développeur.
+        Chaque projet est une occasion de me surpasser, d'apprendre de nouvelles
+        compétences et de relever de nouveaux défis. Ma passion pour la création
+        ne se limite pas seulement au développement, mais se reflète également
+        dans mon approche globale de la vie professionnelle !
+      </p>
+      <a href="#project">Découvrez ce que j'ai fais</a>
+    </section>
+    <!--END PROJECTS BLOC-->
 
-      <div class="box-item">
-        <div class="flip-box">
-          <div class="flip-box-front text-center filter-"
-            style="background-image: url('https://s25.postimg.cc/l2q9ujy4f/cta-4.png');">
-            <div class="inner color-white">
-              <h3 class="flip-box-header">Dedicated</h3>
-              <p>A short sentence describing this callout is.</p>
-              <img src="https://s25.postimg.cc/65hsttv9b/cta-arrow.png" alt="" class="flip-box-img">
-            </div>
-          </div>
-          <div class="flip-box-back text-center"
-            style="background-image: url('https://s25.postimg.cc/l2q9ujy4f/cta-4.png');">
-            <div class="inner color-white">
-              <h3 class="flip-box-header">Dedicated</h3>
-              <p>A short sentence describing this callout is.</p>
-              <button class="flip-box-button">Learn More</button>
-            </div>
-          </div>
-        </div>
-      </div>
+    <!--VITRINE SECTION-->
+    <section class="bg-page px-10">
 
-    </div>
-  </section>
-  <!--END VITRINE SECTION-->
+    </section>
+    <!--END VITRINE SECTION-->
 
-  <!--COMPETENCES SECTION-->
-  <section>
-    <a href="assets/img/cv.pdf" target="blank">
-      <img src="assets/img/cv.png" alt="Visualisation de mon CV" />
-    </a>
+    <!--COMPETENCES SECTION-->
+    <section class="bg-page px-10">
+      <article>
+        <a href="assets/img/cv.pdf" target="blank" class="hover:cursor-pointer">
+          <img src="assets/img/cv.png" alt="Visualisation de mon CV" class="" />
+        </a>
 
-    <h2>Mes compétences</h2>
-    <ul>
+        <h2>Mes compétences</h2>
+        <ul>
 
-      <li>
-        <h3>Perfectionniste</h3>
-        <p>J'ai le sens du détail et j'aime que les choses soit au rendu final exactement comme elles étaient prévues à
-          la base.</p>
-      </li>
+          <li>
+            <h3>Perfectionniste</h3>
+            <p>J'ai le sens du détail et j'aime que les choses soit au rendu final exactement comme elles étaient
+              prévues
+              à
+              la base.</p>
+          </li>
 
-      <li>
-        <h3>Perfectionniste</h3>
-        <p>J'ai le sens du détail et j'aime que les choses soit au rendu final exactement comme elles étaient prévues à
-          la base.</p>
-      </li>
+          <li>
+            <h3>Perfectionniste</h3>
+            <p>J'ai le sens du détail et j'aime que les choses soit au rendu final exactement comme elles étaient
+              prévues
+              à
+              la base.</p>
+          </li>
 
-      <li>
-        <h3>Perfectionniste</h3>
-        <p>J'ai le sens du détail et j'aime que les choses soit au rendu final exactement comme elles étaient prévues à
-          la base.</p>
-      </li>
+          <li>
+            <h3>Perfectionniste</h3>
+            <p>J'ai le sens du détail et j'aime que les choses soit au rendu final exactement comme elles étaient
+              prévues
+              à
+              la base.</p>
+          </li>
 
-      <li>
-        <h3>Perfectionniste</h3>
-        <p>J'ai le sens du détail et j'aime que les choses soit au rendu final exactement comme elles étaient prévues à
-          la base.</p>
-      </li>
+          <li>
+            <h3>Perfectionniste</h3>
+            <p>J'ai le sens du détail et j'aime que les choses soit au rendu final exactement comme elles étaient
+              prévues
+              à
+              la base.</p>
+          </li>
 
-      <li>
-        <h3>Perfectionniste</h3>
-        <p>J'ai le sens du détail et j'aime que les choses soit au rendu final exactement comme elles étaient prévues à
-          la base.</p>
-      </li>
+          <li>
+            <h3>Perfectionniste</h3>
+            <p>J'ai le sens du détail et j'aime que les choses soit au rendu final exactement comme elles étaient
+              prévues
+              à
+              la base.</p>
+          </li>
 
-      <li>
-        <h3>Perfectionniste</h3>
-        <p>J'ai le sens du détail et j'aime que les choses soit au rendu final exactement comme elles étaient prévues à
-          la base.</p>
-      </li>
+          <li>
+            <h3>Perfectionniste</h3>
+            <p>J'ai le sens du détail et j'aime que les choses soit au rendu final exactement comme elles étaient
+              prévues
+              à
+              la base.</p>
+          </li>
 
-    </ul>
-  </section>
-  <!--END --COMPETENCES SECTION-->
+        </ul>
+      </article>
+    </section>
+    <!--END --COMPETENCES SECTION-->
 
-  <!--CONTACT SECTION-->
-  <section>
-    <form action="" method="">
-      <h3>contactez-moi</h3>
-      <input type="text" name="name" placeholder="Votre nom" />
-      <input type="email" name="email" placeholder="Votre email" />
-      <input type="text" name="subject" placeholder="La raison de votre message" />
-      <input type="text" name="email" placeholder="Votre message" />
+    <!--CONTACT SECTION-->
+    <section id="contact" class="bg-end px-10">
+      <form action="" method="">
+        <h3>contactez-moi</h3>
+        <input type="text" name="name" placeholder="Votre nom" />
+        <input type="email" name="email" placeholder="Votre email" />
+        <input type="text" name="subject" placeholder="La raison de votre message" />
+        <input type="text" name="email" placeholder="Votre message" />
 
-      <input type="submit" value="Envoyer">
-    </form>
-  </section>
-  <!--END CONTACT SECTION-->
+        <input type="submit" value="Envoyer">
+      </form>
+    </section>
+    <!--END CONTACT SECTION-->
 
-  <!--FOOTER SECTION-->
-  <footer>
-    <span>Portfolio 2023</span>
-    <br />
-    <span>© 2023 - RENOLLET Nataël</span>
-    <br />
-    <span>Mentions Légales</span>
-
-    <?php include('includes/socialicons.php'); ?>
-  </footer>
-  <!--END FOOTER SECTION-->
+    <!--FOOTER SECTION-->
+    <footer class="bg-default">
+      <?php include('includes/socialicons.php'); ?>
+      <p class="text-center">
+        Portfolio 2023 © RENOLLET Nataël
+        <br>
+        Mentions Légales
+      </p>
+    </footer>
+    <!--END FOOTER SECTION-->
+  </main>
 </body>
 
 </html>
