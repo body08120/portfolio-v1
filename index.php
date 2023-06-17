90bebab38b575b514cf329d03cd1cd4020e68906
body08120 <nataelrenollet08120@gmail.com>
2023-06-17 21:31:12 +0200

    Cards de projet ajoutées, responsive réaliser, suppression section vitrine, et ajout des cards dans la section presentation projet, passage au compétences =)

--- index.php
+++ index.php
@@ -6,7 +6,11 @@
   <meta http-equiv="X-UA-Compatible" content="IE=edge" />
   <meta name="viewport" content="width=device-width, initial-scale=1.0" />
   <title>Renollet Nataël - Développeur web</title>
-  <!--metadescription-->
+  <meta name="description"
+    content="Je suis un développeur web junior passionné par la création d'expériences en ligne. 
+            Ma maîtrise du HTML, CSS et PHP me permet de concevoir des sites web esthétiques et fonctionnels.  
+            En constante évolution, j'ai récemment commencé à explorer le potentiel du JavaScript pour ajouter des fonctionnalités interactives à mes projets.
+            Grâce à ma formation au pôle UIMM, j'ai acquis des compétences solides et je suis prêt à relever de nouveaux défis dans le domaine du développement web.">
 
   <!-- Tailwind CSS -->
   <script src="https://cdn.tailwindcss.com"></script>
@@ -24,7 +28,7 @@
       }
     }
   </script>
-  <style type="text/tailwindcss">
+  <style>
     @layer utilities {
       .bg-header {
         background: linear-gradient(rgba(1, 1, 1, 0.1), rgba(1, 1, 1, 0.1)), url('assets/img/bg/background-header.png');
@@ -32,35 +36,25 @@
         background-repeat: no-repeat;
         background-position: center;
         background-attachment: fixed;
-        /* -webkit-background-size: cover;
-        -moz-background-size: cover;
-        -o-background-size: cover; */
       }
-        .bg-page {
-        background: linear-gradient(rgba(1, 1, 1, 0.1), rgba(1, 1, 1, 0.1)), url('assets/img/bg/background-page.png'); 
+
+      .bg-page {
+        background: linear-gradient(rgba(1, 1, 1, 0.1), rgba(1, 1, 1, 0.1)), url('assets/img/bg/background-page.png');
         background-size: cover;
         background-repeat: no-repeat;
         background-position: coutain;
         background-attachment: fixed;
-        /* filter: brightness(50%) */
-        /* -webkit-background-size: cover;
-        -moz-background-size: cover;
-        -o-background-size: cover; */
       }
+
       .bg-end {
-        background: linear-gradient(rgba(1, 1, 1, 0.1), rgba(1, 1, 1, 0.1)), url('assets/img/bg/background-end.png'); 
+        background: linear-gradient(rgba(1, 1, 1, 0.1), rgba(1, 1, 1, 0.1)), url('assets/img/bg/background-end.png');
         background-size: cover;
         background-repeat: no-repeat;
         background-position: coutain;
         background-attachment: fixed;
-        /* filter: brightness(50%) */
-        /* -webkit-background-size: cover;
-        -moz-background-size: cover;
-        -o-background-size: cover; */
       }
     }
   </style>
-
 </head>
 
 <body>
@@ -120,8 +114,10 @@
     </header>
     <!--END HEADER SECTION-->
 
+    <!--################################-->
+
     <!--PROJECTS BLOC-->
-    <section class="bg-page px-10 pt-16
+    <section class="bg-page px-10 py-16
                sm:py-16
                lg:py-12
                xl:py-20">
@@ -155,74 +151,139 @@
         </div>
 
       </div>
-    </section>
-    <!--END PROJECTS BLOC-->
 
-    <!--VITRINE SECTION-->
-    <section class="bg-page px-10 py-16
-              sm:py-32
-              lg:py-24
-              xl:py-40">
 
-      <div class="flex justify-around py-4">
+      <!--SECTION CARDS-->
+      <div class="border-y-2 py-4 my-8 border-default sm:flex">
 
         <!--     primera card -->
-        <div class="bg-white w-72 h-96 shadow-md rounded m-3">
+        <div class="h-full bg-white shadow-md rounded m-4">
           <div class="h-3/4 w-full">
-            <img class="w-full h-full object-cover rounded-t"
-              src="assets/img/wfbcorp.png"
-              alt="piña">
+            <img class="w-full h-full object-cover rounded-t" src="assets/img/wfbcorp.png" alt="piña"
+              onclick="showModal('assets/img/wfbcorp.png')" />
           </div>
           <div class="w-full h-1/4 p-3">
             <a href="#" class=" hover:text-yellow-600 text-gray-700">
               <span class="text-lg font-semibold uppercase tracking-wide ">Pineapple</span>
             </a>
-            <p class="text-gray-600 text-sm leading-5 mt-1">Lorem ipsum dolor sit amet, consectetur adipiscing elit.</p>
+            <p class="text-gray-600 text-sm leading-5 mt-1
+            md:text-base
+            xl:text-lg">Lorem ipsum dolor sit amet, consectetur adipiscing elit.</p>
+
+            <div class="py-2 text-white text-xs text-center grid grid-cols-2 gap-2
+            md:text-sm
+            xl:text-base">
+
+              <span class="bg-default rounded-full px-2">HTML/CSS</span>
+              <span class="bg-default rounded-full px-2">PHP</span>
+              <span class="bg-default rounded-full px-2">JAVASCRIPT</span>
+            </div>
           </div>
         </div>
+
         <!--     segunda card -->
-        <div class="bg-white w-72 h-96 shadow-md rounded m-3">
+        <div class="h-full bg-white shadow-md rounded m-4">
           <div class="h-3/4 w-full">
-            <img class="w-full h-full object-cover rounded-t"
-              src="assets/img/wfbcorp.png"
-              alt="piña">
+            <img class="w-full h-full object-cover rounded-t" src="assets/img/wfbcorp.png" alt="piña"
+              onclick="showModal('assets/img/wfbcorp.png')" />
           </div>
           <div class="w-full h-1/4 p-3">
             <a href="#" class=" hover:text-yellow-600 text-gray-700">
               <span class="text-lg font-semibold uppercase tracking-wide ">Banana</span>
             </a>
-            <p class="text-gray-600 text-sm leading-5 mt-1">Lorem ipsum dolor sit amet, consectetur adipiscing elit.</p>
+            <p class="text-gray-600 text-sm leading-5 mt-1
+            md:text-base
+            xl:text-lg">Lorem ipsum dolor sit amet, consectetur adipiscing elit.</p>
+
+            <div class="py-2 text-white text-xs text-center grid grid-cols-2 gap-2
+            md:text-sm
+            xl:text-base">
+
+              <span class="bg-default rounded-full px-2">HTML/CSS</span>
+              <span class="bg-default rounded-full px-2">PHP</span>
+              <span class="bg-default rounded-full px-2">JAVASCRIPT</span>
+            </div>
           </div>
         </div>
+
         <!--     tercera card -->
-        <div class="bg-white w-72 h-96 shadow-md rounded m-3">
+        <div class="hidden h-full bg-white shadow-md rounded m-4 lg:block">
           <div class="h-3/4 w-full">
-            <img class="w-full h-full object-cover rounded-t"
-              src="assets/img/wfbcorp.png"
-              alt="piña">
+            <img class="w-full h-full object-cover rounded-t" src="assets/img/wfbcorp.png" alt="piña"
+              onclick="showModal('assets/img/wfbcorp.png')" />
           </div>
           <div class="w-full h-1/4 p-3">
             <a href="#" class=" hover:text-yellow-600 text-gray-700">
               <span class="text-lg font-semibold uppercase tracking-wide ">Mango</span>
             </a>
-            <p class="text-gray-600 text-sm leading-5 mt-1">Lorem ipsum dolor sit amet, consectetur adipiscing elit.</p>
+            <p class="text-gray-600 text-sm leading-5 mt-1
+            md:text-base
+            xl:text-lg">Lorem ipsum dolor sit amet, consectetur adipiscing elit.</p>
+
+            <div class="py-2 text-white text-xs text-center grid grid-cols-2 gap-2
+            md:text-sm
+            xl:text-base">
+
+              <span class="bg-default rounded-full px-2">HTML/CSS</span>
+              <span class="bg-default rounded-full px-2">PHP</span>
+              <span class="bg-default rounded-full px-2">JAVASCRIPT</span>
+            </div>
+          </div>
+        </div>
+
+        <!-- cards -->
+        <div class="hidden h-full bg-white shadow-md rounded m-4 2xl:block">
+          <div class="h-3/4 w-full">
+            <img class="w-full h-full object-cover rounded-t" src="assets/img/wfbcorp.png" alt="piña"
+              onclick="showModal('assets/img/wfbcorp.png')" />
+          </div>
+          <div class="w-full h-1/4 p-3">
+            <a href="#" class=" hover:text-yellow-600 text-gray-700">
+              <span class="text-lg font-semibold uppercase tracking-wide ">Mango</span>
+            </a>
+            <p class="text-gray-600 text-sm leading-5 mt-1
+            md:text-base
+            xl:text-lg">Lorem ipsum dolor sit amet, consectetur adipiscing elit.</p>
+
+            <div class="py-2 text-white text-xs text-center grid grid-cols-2 gap-2
+            md:text-sm
+            xl:text-base">
+
+              <span class="bg-default rounded-full px-2">HTML/CSS</span>
+              <span class="bg-default rounded-full px-2">PHP</span>
+              <span class="bg-default rounded-full px-2">JAVASCRIPT</span>
+            </div>
           </div>
         </div>
 
       </div>
 
 
+      <!-- The Modal -->
+      <div id="modal"
+        class="hidden fixed top-0 left-0 z-80 w-screen h-screen bg-black/70 flex justify-center items-center">
+
+        <!-- The close button -->
+        <a class="fixed z-90 top-6 right-8 text-white text-5xl font-bold" href="javascript:void(0)"
+          onclick="closeModal()">&times;</a>
+
+        <!-- A big image will be displayed here -->
+        <img id="modal-img" class="max-w-[800px] max-h-[600px] object-cover" src="#" alt="modal" />
+      </div>
+
     </section>
-    <!--END VITRINE SECTION-->
+    <!--END PROJECTS BLOC-->
+
+    <!--################################-->
 
     <!--COMPETENCES SECTION-->
-    <section class="bg-page px-10 pt-16
-              sm:pt-32
-              lg:pt-24
-              xl:pt-40">
+    <section class="bg-page px-10 py-16
+               sm:py-16
+               lg:py-12
+               xl:py-20">
       <article>
         <a href="assets/img/cv.pdf" target="blank" class="hover:cursor-pointer">
-          <img src="assets/img/cv.png" alt="Visualisation de mon CV" class="" />
+          <img src="assets/img/cv.png" alt="Visualisation de mon CV" />
         </a>
 
         <h2>Mes compétences</h2>
@@ -281,11 +342,13 @@
     </section>
     <!--END --COMPETENCES SECTION-->
 
+    <!--################################-->
+
     <!--CONTACT SECTION-->
-    <section id="contact" class="bg-end px-10 pt-16
-              sm:pt-32
-              lg:pt-24
-              xl:pt-40">
+    <section id="contact" class="bg-end px-10 py-16
+               sm:py-16
+               lg:py-12
+               xl:py-20">
       <form action="" method="">
         <h3>contactez-moi</h3>
         <input type="text" name="name" placeholder="Votre nom" />
@@ -298,6 +361,8 @@
     </section>
     <!--END CONTACT SECTION-->
 
+    <!--################################-->
+
     <!--FOOTER SECTION-->
     <footer class="bg-default">
       <?php include('includes/socialicons.php'); ?>
@@ -308,7 +373,10 @@
       </p>
     </footer>
     <!--END FOOTER SECTION-->
+
   </main>
+
+  <script src="assets/js/img-modal-cards.js"></script>
 </body>
 
 </html>
