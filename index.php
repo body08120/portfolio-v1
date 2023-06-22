fb311e5f0324ec4da8771b87cb8d7f7a8dfbd8b6
body08120 <nataelrenollet08120@gmail.com>
2023-06-22 13:37:01 +0200

    compétence et contact

--- index.php
+++ index.php
@@ -117,20 +117,19 @@
     <!--################################-->
 
     <!--PROJECTS BLOC-->
-    <section class="bg-page px-10 py-16
+    <section class="bg-page px-10 py-4
                sm:py-16
                lg:py-12
                xl:py-20">
-
-      <div class="mx-auto text-justify
+      <div class="mx-auto text-justify py-16
               max-sm:text-center
               lg:w-3/4">
 
         <h2 class="my-4 font-thin text-title-color text-4xl uppercase tracking-wider
                  sm:text-5xl
                  lg:my-8 lg:text-7xl lg:w-3/4
-                 xl:my-12 xl:w-7/12
-                 2xl:text-8xl 2x:5/12">Je créer avec passion !</h2>
+                 xl:my-12 xl:w-5/12
+                 ">Je créer avec passion !</h2>
 
         <p class="my-4 text-lg font-light tracking-widest
                 sm:text-xl
@@ -149,127 +148,135 @@
                                 lg:tracking-widest lg:text-xl lg:text-right
                                 xl:text-1xl xl:py-4 xl:px-6">Découvrir</a>
         </div>
-
       </div>
 
 
       <!--SECTION CARDS-->
-      <div class="border-y-2 py-4 my-8 border-default sm:flex">
+      <section class="border-y-2 py-4 my-8 border-default text-center lg:px-16">
 
-        <!--     primera card -->
-        <div class="h-full bg-white shadow-md rounded m-4">
-          <div class="h-3/4 w-full">
-            <img class="w-full h-full object-cover rounded-t" src="assets/img/wfbcorp.png" alt="piña"
-              onclick="showModal('assets/img/wfbcorp.png')" />
-          </div>
-          <div class="w-full h-1/4 p-3">
-            <a href="#" class=" hover:text-yellow-600 text-gray-700">
-              <span class="text-lg font-semibold uppercase tracking-wide ">Pineapple</span>
-            </a>
-            <p class="text-gray-600 text-sm leading-5 mt-1
+        <h2 class="my-4 font-thin text-title-color text-4xl uppercase tracking-wider
+               sm:text-5xl
+               lg:my-8 lg:text-7xl
+               xl:my-12
+               ">Quelques-uns de mes projets !</h2>
+
+        <div class="sm:flex">
+
+          <!-- card -->
+          <div class="h-full bg-white shadow-md rounded m-4">
+            <div class="h-3/4 w-full">
+              <img class="w-full h-full object-cover rounded-t" src="assets/img/wfbcorp.png" alt="piña"
+                onclick="showModal('assets/img/wfbcorp.png')" />
+            </div>
+            <div class="w-full h-1/4 p-3">
+              <a href="#" class=" hover:text-yellow-600 text-gray-700">
+                <span class="text-lg font-semibold uppercase tracking-wide ">Pineapple</span>
+              </a>
+              <p class="text-gray-600 text-sm leading-5 mt-1
             md:text-base
             xl:text-lg">Lorem ipsum dolor sit amet, consectetur adipiscing elit.</p>
 
-            <div class="py-2 text-white text-xs text-center grid grid-cols-2 gap-2
+              <div class="py-2 text-white text-xs text-center grid grid-cols-2 gap-2
             md:text-sm
             xl:text-base">
 
-              <span class="bg-default rounded-full px-2">HTML/CSS</span>
-              <span class="bg-default rounded-full px-2">PHP</span>
-              <span class="bg-default rounded-full px-2">JAVASCRIPT</span>
+                <span class="bg-default rounded-full px-2">HTML/CSS</span>
+                <span class="bg-default rounded-full px-2">PHP</span>
+                <span class="bg-default rounded-full px-2">JAVASCRIPT</span>
+              </div>
             </div>
           </div>
-        </div>
 
-        <!--     segunda card -->
-        <div class="h-full bg-white shadow-md rounded m-4">
-          <div class="h-3/4 w-full">
-            <img class="w-full h-full object-cover rounded-t" src="assets/img/wfbcorp.png" alt="piña"
-              onclick="showModal('assets/img/wfbcorp.png')" />
-          </div>
-          <div class="w-full h-1/4 p-3">
-            <a href="#" class=" hover:text-yellow-600 text-gray-700">
-              <span class="text-lg font-semibold uppercase tracking-wide ">Banana</span>
-            </a>
-            <p class="text-gray-600 text-sm leading-5 mt-1
+          <!-- card -->
+          <div class="h-full bg-white shadow-md rounded m-4">
+            <div class="h-3/4 w-full">
+              <img class="w-full h-full object-cover rounded-t" src="assets/img/wfbcorp.png" alt="piña"
+                onclick="showModal('assets/img/wfbcorp.png')" />
+            </div>
+            <div class="w-full h-1/4 p-3">
+              <a href="#" class=" hover:text-yellow-600 text-gray-700">
+                <span class="text-lg font-semibold uppercase tracking-wide ">Banana</span>
+              </a>
+              <p class="text-gray-600 text-sm leading-5 mt-1
             md:text-base
             xl:text-lg">Lorem ipsum dolor sit amet, consectetur adipiscing elit.</p>
 
-            <div class="py-2 text-white text-xs text-center grid grid-cols-2 gap-2
+              <div class="py-2 text-white text-xs text-center grid grid-cols-2 gap-2
             md:text-sm
             xl:text-base">
 
-              <span class="bg-default rounded-full px-2">HTML/CSS</span>
-              <span class="bg-default rounded-full px-2">PHP</span>
-              <span class="bg-default rounded-full px-2">JAVASCRIPT</span>
+                <span class="bg-default rounded-full px-2">HTML/CSS</span>
+                <span class="bg-default rounded-full px-2">PHP</span>
+                <span class="bg-default rounded-full px-2">JAVASCRIPT</span>
+              </div>
             </div>
           </div>
-        </div>
 
-        <!--     tercera card -->
-        <div class="hidden h-full bg-white shadow-md rounded m-4 lg:block">
-          <div class="h-3/4 w-full">
-            <img class="w-full h-full object-cover rounded-t" src="assets/img/wfbcorp.png" alt="piña"
-              onclick="showModal('assets/img/wfbcorp.png')" />
-          </div>
-          <div class="w-full h-1/4 p-3">
-            <a href="#" class=" hover:text-yellow-600 text-gray-700">
-              <span class="text-lg font-semibold uppercase tracking-wide ">Mango</span>
-            </a>
-            <p class="text-gray-600 text-sm leading-5 mt-1
+          <!-- card -->
+          <div class="hidden h-full bg-white shadow-md rounded m-4 lg:block">
+            <div class="h-3/4 w-full">
+              <img class="w-full h-full object-cover rounded-t" src="assets/img/wfbcorp.png" alt="piña"
+                onclick="showModal('assets/img/wfbcorp.png')" />
+            </div>
+            <div class="w-full h-1/4 p-3">
+              <a href="#" class=" hover:text-yellow-600 text-gray-700">
+                <span class="text-lg font-semibold uppercase tracking-wide ">Mango</span>
+              </a>
+              <p class="text-gray-600 text-sm leading-5 mt-1
             md:text-base
             xl:text-lg">Lorem ipsum dolor sit amet, consectetur adipiscing elit.</p>
 
-            <div class="py-2 text-white text-xs text-center grid grid-cols-2 gap-2
+              <div class="py-2 text-white text-xs text-center grid grid-cols-2 gap-2
             md:text-sm
             xl:text-base">
 
-              <span class="bg-default rounded-full px-2">HTML/CSS</span>
-              <span class="bg-default rounded-full px-2">PHP</span>
-              <span class="bg-default rounded-full px-2">JAVASCRIPT</span>
+                <span class="bg-default rounded-full px-2">HTML/CSS</span>
+                <span class="bg-default rounded-full px-2">PHP</span>
+                <span class="bg-default rounded-full px-2">JAVASCRIPT</span>
+              </div>
             </div>
           </div>
-        </div>
 
-        <!-- cards -->
-        <div class="hidden h-full bg-white shadow-md rounded m-4 2xl:block">
-          <div class="h-3/4 w-full">
-            <img class="w-full h-full object-cover rounded-t" src="assets/img/wfbcorp.png" alt="piña"
-              onclick="showModal('assets/img/wfbcorp.png')" />
-          </div>
-          <div class="w-full h-1/4 p-3">
-            <a href="#" class=" hover:text-yellow-600 text-gray-700">
-              <span class="text-lg font-semibold uppercase tracking-wide ">Mango</span>
-            </a>
-            <p class="text-gray-600 text-sm leading-5 mt-1
+          <!-- cards -->
+          <div class="hidden h-full bg-white shadow-md rounded m-4 2xl:block">
+            <div class="h-3/4 w-full">
+              <img class="w-full h-full object-cover rounded-t" src="assets/img/wfbcorp.png" alt="piña"
+                onclick="showModal('assets/img/wfbcorp.png')" />
+            </div>
+            <div class="w-full h-1/4 p-3">
+              <a href="#" class=" hover:text-yellow-600 text-gray-700">
+                <span class="text-lg font-semibold uppercase tracking-wide ">Mango</span>
+              </a>
+              <p class="text-gray-600 text-sm leading-5 mt-1
             md:text-base
             xl:text-lg">Lorem ipsum dolor sit amet, consectetur adipiscing elit.</p>
 
-            <div class="py-2 text-white text-xs text-center grid grid-cols-2 gap-2
+              <div class="py-2 text-white text-xs text-center grid grid-cols-2 gap-2
             md:text-sm
             xl:text-base">
 
-              <span class="bg-default rounded-full px-2">HTML/CSS</span>
-              <span class="bg-default rounded-full px-2">PHP</span>
-              <span class="bg-default rounded-full px-2">JAVASCRIPT</span>
+                <span class="bg-default rounded-full px-2">HTML/CSS</span>
+                <span class="bg-default rounded-full px-2">PHP</span>
+                <span class="bg-default rounded-full px-2">JAVASCRIPT</span>
+              </div>
             </div>
           </div>
+
         </div>
 
-      </div>
 
+        <!-- The Modal -->
+        <div id="modal"
+          class="hidden fixed top-0 left-0 z-80 w-screen h-screen bg-black/70 flex justify-center items-center">
 
-      <!-- The Modal -->
-      <div id="modal"
-        class="hidden fixed top-0 left-0 z-80 w-screen h-screen bg-black/70 flex justify-center items-center">
+          <!-- The close button -->
+          <a class="fixed z-90 top-6 right-8 text-white text-5xl font-bold" href="javascript:void(0)"
+            onclick="closeModal()">&times;</a>
 
-        <!-- The close button -->
-        <a class="fixed z-90 top-6 right-8 text-white text-5xl font-bold" href="javascript:void(0)"
-          onclick="closeModal()">&times;</a>
-
-        <!-- A big image will be displayed here -->
-        <img id="modal-img" class="max-w-[800px] max-h-[600px] object-cover" src="#" alt="modal" />
-      </div>
+          <!-- A big image will be displayed here -->
+          <img id="modal-img" class="max-w-[800px] max-h-[600px] object-cover" src="#" alt="modal" />
+        </div>
+      </section>
 
     </section>
     <!--END PROJECTS BLOC-->
@@ -277,67 +284,134 @@
     <!--################################-->
 
     <!--COMPETENCES SECTION-->
-    <section class="bg-page px-10 py-16
-               sm:py-16
+    <section class="bg-page px-10 py-4
                lg:py-12
                xl:py-20">
-      <article>
-        <a href="assets/img/cv.pdf" target="blank" class="hover:cursor-pointer">
-          <img src="assets/img/cv.png" alt="Visualisation de mon CV" />
-        </a>
 
-        <h2>Mes compétences</h2>
-        <ul>
+      <article class="lg:flex flex-row-reverse justify-around">
 
-          <li>
-            <h3>Perfectionniste</h3>
-            <p>J'ai le sens du détail et j'aime que les choses soit au rendu final exactement comme elles étaient
-              prévues
-              à
-              la base.</p>
-          </li>
+        <div class="lg:w-6/12">
 
-          <li>
-            <h3>Perfectionniste</h3>
-            <p>J'ai le sens du détail et j'aime que les choses soit au rendu final exactement comme elles étaient
-              prévues
-              à
-              la base.</p>
-          </li>
+          <h2 class="font-thin text-title-color text-4xl uppercase tracking-wider
+               lg:text-5xl
+               xl:text-6xl">Mes compétences</h2>
+          <br>
 
-          <li>
-            <h3>Perfectionniste</h3>
-            <p>J'ai le sens du détail et j'aime que les choses soit au rendu final exactement comme elles étaient
-              prévues
-              à
-              la base.</p>
-          </li>
+          <ul class="md:grid md:grid-cols-2">
 
-          <li>
-            <h3>Perfectionniste</h3>
-            <p>J'ai le sens du détail et j'aime que les choses soit au rendu final exactement comme elles étaient
-              prévues
-              à
-              la base.</p>
-          </li>
+            <li class="py-4 px-4 text-justify">
+              <h3 class="uppercase font-semibold tracking-wider text-title-color text-lg
+                        md:text-xl
+                        lg:text-1xl">Perfectionniste</h3>
+              <p class="text-text-color text-sm leading-5 mt-1
+            md:text-base
+            xl:text-lg">J'ai le sens du détail et j'aime que les choses soit au rendu final exactement comme elles
+                étaient
+                prévues
+                à
+                la base.</p>
+            </li>
 
-          <li>
-            <h3>Perfectionniste</h3>
-            <p>J'ai le sens du détail et j'aime que les choses soit au rendu final exactement comme elles étaient
-              prévues
-              à
-              la base.</p>
-          </li>
+            <li class="py-4 px-4 text-justify">
+              <h3 class="uppercase font-semibold tracking-wider text-title-color text-lg
+                        md:text-xl
+                        lg:text-1xl">Perfectionniste</h3>
+              <p class="text-text-color text-sm leading-5 mt-1
+            md:text-base
+            xl:text-lg">J'ai le sens du détail et j'aime que les choses soit au rendu final exactement comme elles
+                étaient
+                prévues
+                à
+                la base.</p>
+            </li>
 
-          <li>
-            <h3>Perfectionniste</h3>
-            <p>J'ai le sens du détail et j'aime que les choses soit au rendu final exactement comme elles étaient
-              prévues
-              à
-              la base.</p>
-          </li>
+            <li class="py-4 px-4 text-justify">
+              <h3 class="uppercase font-semibold tracking-wider text-title-color text-lg
+                        md:text-xl
+                        lg:text-1xl">Perfectionniste</h3>
+              <p class="text-text-color text-sm leading-5 mt-1
+            md:text-base
+            xl:text-lg">J'ai le sens du détail et j'aime que les choses soit au rendu final exactement comme elles
+                étaient
+                prévues
+                à
+                la base.</p>
+            </li>
 
-        </ul>
+            <li class="py-4 px-4 text-justify">
+              <h3 class="uppercase font-semibold tracking-wider text-title-color text-lg
+                        md:text-xl
+                        lg:text-1xl">Perfectionniste</h3>
+              <p class="text-text-color text-sm leading-5 mt-1
+            md:text-base
+            xl:text-lg">J'ai le sens du détail et j'aime que les choses soit au rendu final exactement comme elles
+                étaient
+                prévues
+                à
+                la base.</p>
+            </li>
+
+            <li class="py-4 px-4 text-justify">
+              <h3 class="uppercase font-semibold tracking-wider text-title-color text-lg
+                        md:text-xl
+                        lg:text-1xl">Perfectionniste</h3>
+              <p class="text-text-color text-sm leading-5 mt-1
+            md:text-base
+            xl:text-lg">J'ai le sens du détail et j'aime que les choses soit au rendu final exactement comme elles
+                étaient
+                prévues
+                à
+                la base.</p>
+            </li>
+
+            <li class="py-4 px-4 text-justify">
+              <h3 class="uppercase font-semibold tracking-wider text-title-color text-lg
+                        md:text-xl
+                        lg:text-1xl">Perfectionniste</h3>
+              <p class="text-text-color text-sm leading-5 mt-1
+            md:text-base
+            xl:text-lg">J'ai le sens du détail et j'aime que les choses soit au rendu final exactement comme elles
+                étaient
+                prévues
+                à
+                la base.</p>
+            </li>
+
+            <li class="hidden py-4 px-4 text-justify xl:block">
+              <h3 class="uppercase font-semibold tracking-wider text-title-color text-lg
+                        md:text-xl
+                        lg:text-1xl">Perfectionniste</h3>
+              <p class="text-text-color text-sm leading-5 mt-1
+            md:text-base
+            xl:text-lg">J'ai le sens du détail et j'aime que les choses soit au rendu final exactement comme elles
+                étaient
+                prévues
+                à
+                la base.</p>
+            </li>
+
+            <li class="hidden py-4 px-4 text-justify xl:block">
+              <h3 class="uppercase font-semibold tracking-wider text-title-color text-lg
+                        md:text-xl
+                        lg:text-1xl">Perfectionniste</h3>
+              <p class="text-text-color text-sm leading-5 mt-1
+            md:text-base
+            xl:text-lg">J'ai le sens du détail et j'aime que les choses soit au rendu final exactement comme elles
+                étaient
+                prévues
+                à
+                la base.</p>
+            </li>
+
+          </ul>
+        </div>
+
+        <div class="lg:w-5/12">
+          <a href="assets/img/cv.pdf" target="blank" class="hover:cursor-pointer">
+            <img src="assets/img/cv.png" alt="Visualisation de mon CV" />
+          </a>
+        </div>
+
       </article>
     </section>
     <!--END --COMPETENCES SECTION-->
@@ -349,14 +423,24 @@
                sm:py-16
                lg:py-12
                xl:py-20">
-      <form action="" method="">
-        <h3>contactez-moi</h3>
-        <input type="text" name="name" placeholder="Votre nom" />
-        <input type="email" name="email" placeholder="Votre email" />
-        <input type="text" name="subject" placeholder="La raison de votre message" />
-        <input type="text" name="email" placeholder="Votre message" />
 
-        <input type="submit" value="Envoyer">
+      <form action="" method="" class="bg-title-color rounded-xl py-10 px-12 w-max">
+        <h3 class="font-thin text-default text-4xl uppercase tracking-wider
+               lg:text-5xl
+               xl:text-6xl">contactez-moi</h3>
+               <br>
+        <div class="flex flex-col w-full">
+          <input type="text" name="name" placeholder="Votre nom" class="bg-text-color rounded-lg py-2 my-2 text-default" />
+          <input type="email" name="email" placeholder="Votre email" class="bg-text-color rounded-lg py-2 my-2 text-default" />
+          <input type="text" name="subject" placeholder="La raison de votre message" class="bg-text-color rounded-lg py-2 my-2 text-default" />
+          <textarea name="message" id="message" cols="30" rows="10" placeholder="Tapez votre message ici !"
+            class="bg-text-color rounded-lg py-2 my-2 text-default"></textarea>
+
+          <input type="submit" value="ENVOYER" class="bg-default py-2 px-4 rounded-lg uppercase text-center
+                                hover:font-semibold hover:bg-h-button
+                                lg:tracking-widest lg:text-xl
+                                xl:text-1xl xl:py-4 xl:px-6">
+        </div>
       </form>
     </section>
     <!--END CONTACT SECTION-->
